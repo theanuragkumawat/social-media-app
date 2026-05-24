8b146a71ba4b677a359253162f0b7db426f37e00
Anurag Kumawat <anurag.kmwt7851@gmail.com>
2026-05-24 18:07:11 +0530

    feat: update multer middleware to use dynamic temp directory and adjust file storage path

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,13 +1,18 @@
-import multer from "multer"
+import fs from "fs";
+import path from "path";
+
+const tempDir = path.join(process.cwd(), "public/temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    cb(null, tempDir);
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.originalname)
-  }
-})
 
-export const upload = multer({ storage: storage })
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
